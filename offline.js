3b992e3a3557b63ac023ced25d14ded0d7f3b41f
khaisa2008 <161662782+khaisa2008@users.noreply.github.com>
2024-02-29 09:04:25 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1709169259,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/bird-sheet1.png",
+		"images/pipa-sheet0.png",
+		"images/pipaatas-sheet0.png",
+		"images/lantai-sheet0.png",
+		"images/bgf-sheet0.png",
+		"images/playremovebgpreview-sheet0.png",
+		"images/homeremovebgpreview-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
